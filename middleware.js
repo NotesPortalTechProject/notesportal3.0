cabb95d895e6bb3efb8caae5bfb0af5b77fda8fb
aryachawan <aryachawan05@gmail.com>
2025-10-01 19:18:05 +0530

    last active functionality implemented

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers";
 import { decrypt } from "./lib/session";
 import { NextResponse } from "next/server";
+import { supabase } from "./lib/supabaseClient";
 
 const publicRoutes = ["/", "/signup", "/login", "/login/with-otp"]
 export default async function middleware(req) {
@@ -19,6 +20,8 @@ export default async function middleware(req) {
     }
 
     if (isPublicRoute && session?.userId) {
+        const now = new Date().toISOString(); 
+        await supabase.from("users").update({"lastactive":now}).eq("id",session.userId);
         return NextResponse.redirect(new URL(`/${session?.userId}/home`, req.nextUrl));
     }
 
