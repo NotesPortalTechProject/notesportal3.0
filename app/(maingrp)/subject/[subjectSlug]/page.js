cd35c0b3f10dece695e8cda4cc498095e666dae4
aryachawan <aryachawan05@gmail.com>
2025-11-19 14:52:49 +0530

    direct subject sharing functionality (temporary)

---
+++ app/(maingrp)/subject/[subjectSlug]/page.js
@@ -0,0 +1,17 @@
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import { decrypt } from "@/lib/session";
+
+
+export default async function FilePage({ params }) {
+  const { subjectname } = params;
+
+  const cookie = (await cookies()).get("session")?.value;
+  const session = await decrypt(cookie);
+
+  if (!session?.userId) {
+    return redirect("/login");
+  }
+
+  return redirect(`/${userid}/subject/${subjectname}`);
+}
