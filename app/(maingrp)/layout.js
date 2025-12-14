cb0835e741c7442cb216a093a5177a2902840e2c
aryachawan <aryachawan05@gmail.com>
2025-12-14 19:09:13 +0530

    main bug patch for Id removal

--- app/(maingrp)/layout.js
+++ app/(maingrp)/layout.js
@@ -4,7 +4,6 @@ import { getUserData } from "@/lib/data-fetch-functions";
 import { decrypt } from "@/lib/session";
 import { redirect } from "next/dist/server/api-utils";
 import { cookies } from "next/headers";
-import UserProvider from "./context/userProvider";
 
 export default async function MainLayout({ children }) {
     const cookie = cookies();
@@ -17,18 +16,16 @@ export default async function MainLayout({ children }) {
     const userSession = await decrypt(session.value);
     const userdata = await getUserData(userSession.userId)
     return (
-        <UserProvider value={userSession}>
-            <div className="flex flex-col h-screen bg-[#0d0b14] text-white">
-                <HorizontalNavbar id={userSession.userId} />
-                <div className="flex flex-1 overflow-hidden">
-                    <VerticalNavbar id={userSession.userId} subjectlist={JSON.parse(userdata['subjects'])} />
-                    <main className="flex-1 overflow-y-auto p-6 bg-[#1a1a1a] scrollbar-hide">
-                        {children}
-                    </main>
-                </div>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9497779713832911"
-                    crossorigin="anonymous"></script>
+        <div className="flex flex-col h-screen bg-[#0d0b14] text-white">
+            <HorizontalNavbar id={userSession.userId} />
+            <div className="flex flex-1 overflow-hidden">
+                <VerticalNavbar id={userSession.userId} subjectlist={JSON.parse(userdata['subjects'])} />
+                <main className="flex-1 overflow-y-auto p-6 bg-[#1a1a1a] scrollbar-hide">
+                    {React.cloneElement(children,{userId:userSession.userId})}
+                </main>
             </div>
-        </UserProvider>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9497779713832911"
+                crossorigin="anonymous"></script>
+        </div>
     );
 }
